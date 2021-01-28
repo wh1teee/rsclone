a398bb7054b4527a909f9ff44f6b8aa46da27eee
Danil Kohan <Sportodavka@gmail.com>
2021-01-28 22:16:58 +0300

    feat: create template 4

---
+++ src/templates/template4.js
@@ -0,0 +1,46 @@
+import './temp4.scss'
+import image2 from '../images/Images/templates4/men.jpg'
+const template4 = `
+                <section class="left_section">
+                    <div class="photo">
+                        <img src=${image2} class="img-men"></img></div>
+                    <div class="header">
+                        <h2 class="name_persone">Aaron Loeb</h2>
+                        <h3 class="profession">PRESCHOOL TEACHER</h3>
+                     </div>
+                    <div class="career_person">
+                        <h3 class="career_title">
+                        CAREER GOALS
+                         </h3>
+                         <p class="career_text">
+                            I create fun and valuabie Mathematics.<br>
+                            English,and Science lessons for children of<br>
+                            all ages.I aim to teach preschool children in<br>
+                            remote areas.
+                        </p>
+                        </div>
+                        <div class="interests_person">
+                            <h3 class="interests_person_title">
+                            MAIN INTERESTS
+                            </h3>
+                            <ul class="interests_person_list">
+                            <li>Entertaining Lesson Plans for kids</li>
+                            <li>Art in Mathematics</li>
+                            <li>Literature</li>
+                            <li>Online Tutoring</li>
+                            <li>Digital Education</li>
+                            <li>Public Speaking</li>
+                            <li>Spoken Word Poetry</li>
+                            </ul>
+                        </div>
+                </section>
+
+    <section class="right_section">
+    <div class="title">
+    <h3>
+    ACADEMIC HISTORY
+    </h3>
+    </div>
+    </section>
+`;
+export default template4;

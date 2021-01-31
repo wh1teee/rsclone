0f5b78e84b399df7a84fdb2779c17cbc0aa74b2a
Danil Kohan <Sportodavka@gmail.com>
2021-01-31 19:00:18 +0300

    feat: finished temp4

--- src/templates/template4.js
+++ src/templates/template4.js
@@ -1,17 +1,18 @@
 import './temp4.scss'
 import image2 from '../images/Images/templates4/men.jpg'
-const template4 = `
+const template4 = `<footer class="container">
                 <section class="left_section">
                     <div class="photo">
                         <img src=${image2} class="img-men"></img></div>
                     <div class="header">
                         <h2 class="name_persone">Aaron Loeb</h2>
-                        <h3 class="profession">PRESCHOOL TEACHER</h3>
+                        <h4 class="profession">PRESCHOOL TEACHER</h4>
                      </div>
+
                     <div class="career_person">
-                        <h3 class="career_title">
+                        <h4 class="career_title">
                         CAREER GOALS
-                         </h3>
+                         </h4>
                          <p class="career_text">
                             I create fun and valuabie Mathematics.<br>
                             English,and Science lessons for children of<br>
@@ -19,10 +20,11 @@ const template4 = `
                             remote areas.
                         </p>
                         </div>
+
                         <div class="interests_person">
-                            <h3 class="interests_person_title">
+                            <h4 class="interests_person_title">
                             MAIN INTERESTS
-                            </h3>
+                            </h4>
                             <ul class="interests_person_list">
                             <li>Entertaining Lesson Plans for kids</li>
                             <li>Art in Mathematics</li>
@@ -33,14 +35,77 @@ const template4 = `
                             <li>Spoken Word Poetry</li>
                             </ul>
                         </div>
+
+                        <div class="contact_person">
+                            <h4 class="contact_person_title">REACH ME AT</h4>
+                            <a class="left__link1" href="tel:+1 234 56 78 90">Number: 123-456-7890</a><br>
+                            <a class="left__link2" href="mailto: hello@reallygreatsite.com">Email: hello@reallygreatsite.com</a><br>
+                            <a class="left__link3" href="https://www.reallygreatsite.com/" target="_blank">Website: www.reallygreatsite.com</a><br>
+                            <a class="left__link4">Address: 123 Anywhere St., Any City, State, Country 12345</a>
+                        </div>
                 </section>
 
+
     <section class="right_section">
-    <div class="title">
-    <h3>
-    ACADEMIC HISTORY
-    </h3>
-    </div>
+        <div class="history_person">
+            <h4>
+            ACADEMIC HISTORY
+            </h4>
+            <p class="place__name">San Dias University</p>
+            <p class="history_person_list">BA Education I Class Valedictorian</p>
+            <ul>
+                <li>President.Educators Circle</li>
+                <li>Vice President. The SDU Official Writers Club</li>
+                <li>Member, Science Club</li>
+                <li>Member, Physics Org</li>
+            </ul>
+            <p class="place__name">Pedigon High School</p>
+            <p class="history_person_list">Consistant Honor Student News Writer</p>
+            <ul>
+                <li>Head Writer, PHS Express</li>
+                <li>President, The Pedigon High School Writing Committee</li>
+                <li>Head Editor, The PHS Yearbook Committee</li>
+                <li>Member, The PHS Debate Society</li>
+            </ul>
+        </div>
+
+        <div class="career_person_history">
+            <h4 class="career_person_history_title">
+            CAREER HISTORY
+            </h4>
+            <p class="profession_name">Preschool Teacher</p>
+            <p class="career_person_history_list">Kyobi Preschool September 2019 - Present</p>
+            <ul>
+                <li>Serves as the head teacher for one section</li>
+                <li>Teaches art, sclence, and English to preschool<br>
+                    students</li>
+                <li>Coordinates with parents for class field trips</li>
+            </ul>
+            <p class="profession_name">Mathematics Teacher</p>
+            <p class="career_person_history_list">San Dias Kids Preschool I 2018 - 2019</p>
+            <ul>
+                <li>Tauqht math to kids aged 3 to 8</li>
+                <li>Came up with fun and informative math lessons<br>
+                    for students
+                </li>
+                <li>Created new lesson plans for new math<br>
+                    teachers</li>
+            </ul>
+        </div>
+
+        <div class="achievements_person">
+            <h4 class="achievements_person_title">
+            ACHIEVEMENTS
+            </h4>
+            <ul class="achievements_list">
+                <li>Best Preschool Teacher(2020)</li>
+                <li>Most Promising Educator(2019)</li>
+                <li>Employee of the Year(2018)</li>
+                <li>Best Math Teacher(2017)</li>
+                <li>Best Faculty Member(2016)</li>
+            </ul>
+        </div>
     </section>
+</footer>
 `;
 export default template4;

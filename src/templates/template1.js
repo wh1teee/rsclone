981655b6376a7faf9d92173fe7eb07570b38f95d
wh1teee <hemp0@mail.ru>
2021-02-01 22:34:27 +0300

    refactor: add/delete some classes, replacing a tags for p

--- src/templates/template1.js
+++ src/templates/template1.js
@@ -1,101 +1,70 @@
 import './temp1.scss';
 
-const template1 = `<div class="template1__wrapper template__wrapper">
-    <aside class="left__section">
-        <div class="photo"></div>
-                <h4 class="article__title left__title1">Introduction</h4>
-                <p class="article__text left__article1">Hi there! I'm Benjamin and I love teaching all kinds of children.
-                    I love getting to know my students and nurturing them to their fullest potential.</p>
-                <h4 class="article__title left__title2">Professional Skills</h4>
-                <ul  class="article__block left__article2">
-                    <li>English Literature</li>
-                    <li>Grammar and Vocabulary</li>
-                    <li>Student Development</li>
-                    <li>Classroom Management</li>
-                    <li>Extracurricular Activities</li>
-                    <li>Lesson Plan Development</li>
-                    <li>Trained for First Aid</li>
-                </ul>
-                
-                <h4 class="article__title left__title3">Contact Details</h4>
-                    <a class="left__link1" href="tel:+1 234 56 78 90">Number: 123-456-7890</a>
-                    <a class="left__link2" href="mailto: hello@reallygreatsite.com">Email: hello@reallygreatsite.com</a>
-                    <a class="left__link3" href="https://www.reallygreatsite.com/" target="_blank">Website: www.reallygreatsite.com</a>
-                    <a class="left__link4">Address: 123 Anywhere St., Any City, State, Country 12345</a>
-                    
-                <h4 class="article__title left__title4">My Interests</h4>               
-                 <p class="left__text1">Reading and Writing Poetry</p>
-                    <p class="left__text2">Mountain Climbing</p>
-                    <p class="left__text3">Wall Climbing</p>
-                    <p class="left__text4">Jazz and Indie Music</p>
-                    <p class="left__text5">Part-Time Babysitting</p>
-                    <p class="left__text6">Painting and Sketching</p>
-               
-    </aside>
-    <article class="main__section">
-        <section class="header">
-                <h1 class="name text">BENJAMIN SHAH</h1>
-                <h3 class="profession text">ELEMENTARY TEACHER</h3>
-            </div>
-        </section>
-        <section class="main">
-                <div class="person__information">
-                    <h3 class="information__title main__title1">
-                        CAREER PROGRESSION
-                    </h3>
-                    <div class="working__place">
-                        <h2 class="working__place__title">ELEMENTARY TEACHER</h2>
-                        <p class="place__name">Gunny Rings Elementary School, 2012-Present</p>
-                        <div class="description">
-                            <ul>
-                                <li>Co-designed lesson plans for various classes</li>
-                                <li>Taught classes using modern teaching methods</li>
-                                <li>Fostered a supportive and encouraging classroom environment</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="working__place">
-                        <h3 class="working__place__title">SUBSTITUTE TEACHER</h3>
-                        <p class="place__name">Gresdale Elementary School, 2011-2012</p>
-                        <div class="description">
-                            <ul>
-                                <li>Substituted for English and Art classes</li>
-                                <li>Provided after-school tutoring for students</li>
-                                <li>Taught English summer school classes for elementary students</li>
-                            </ul>
-                        </div>
-                    </div>
+const template1 = `
+<div class="template1__wrapper template__wrapper">
+    <aside class="left__section">    
+        <div class="moveable photo"></div>
+        
+        <h4 class="moveable article__title text1">Introduction</h4>
+        <p class="moveable text2">Hi there! I'm Benjamin and I love teaching all kinds of children.
+            I love getting to know my students and nurturing them to their fullest potential.</p>
+            
+        <h4 class="moveable article__title text3">Professional Skills</h4>
+        <p  class="moveable article__block text4">English Literature</p>
+        <p  class="moveable article__block text5">Grammar and Vocabulary</p>
+        <p  class="moveable article__block text6">Student Development</p>
+        <p  class="moveable article__block text7">Classroom Management</p>
+        <p  class="moveable article__block text8">Extracurricular Activities</p>
+        <p  class="moveable article__block text9">Lesson Plan Development</p>
+        <p  class="moveable article__block text10">Trained for First Aid</p>        
 
-                </div>
-                <div class="person__information">
-                    <h3 class="information__title">
-                        ACADEMIC BACKGROUND
-                    </h3>
-                    <div class="working__place">
-                        <h3 class="working__place__title">OULCASTER UNIVERSITY</h3>
-                        <p class="place__name">MA in Education & Development (2010)</p>
-                        <div class="description">
-                            <ul>
-                                <li>Graduated with First Honors</li>
-                                <li>Published a paper entitled Education from a Behavioral Standpoint</li>
-                                <li>Successfully organized a state-funded teaching program</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="working__place">
-                        <h3 class="working__place__title">KIRMARE STATE COLLEGE</h3>
-                        <p class="place__name">B.A. in English Literature (2007)</p>
-                        <div class="description">
-                            <ul>
-                                <li>Graduated with 1st Honors</li>
-                                <li>Awarded with Academic Excellence in English</li>
-                                <li>Member of the Kirmere Youth Education Charity</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-        </section>
-    </article>
+        <h4 class="moveable article__title text11">Contact Details</h4>
+        <p class="moveable link text12">Number: 123-456-7890</p>
+        <p class="moveable link text13">Email: hello@reallygreatsite.com</p>
+        <p class="moveable link text14">Website: www.reallygreatsite.com</p>
+        <p class="moveable link text15">Address: 123 Anywhere St., Any City, State, Country 12345</p>
+
+        <h4 class="moveable article__title text16">My Interests</h4>
+        <p class="moveable text17">Reading and Writing Poetry</p>
+        <p class="moveable text18">Mountain Climbing</p>
+        <p class="moveable text19">Wall Climbing</p>
+        <p class="moveable text20">Jazz and Indie Music</p>
+        <p class="moveable text21">Part-Time Babysitting</p>
+        <p class="moveable text22">Painting and Sketching</p>
+    </aside>
+    
+    <aside class="main__section">
+        <section class="header"></section>
+        <h1 class="moveable text23">BENJAMIN SHAH</h1>
+        <h3 class="moveable text24">ELEMENTARY TEACHER</h3>
+
+        <h3 class="moveable text25">CAREER PROGRESSION</h3>
+        <h2 class="moveable working__place__title text26">ELEMENTARY TEACHER</h2>
+        <p class="moveable place__name text27">Gunny Rings Elementary School, 2012-Present</p>
+        
+        <p class="moveable text28">Co-designed lesson plans for various classes</p>
+        <p class="moveable text29">Taught classes using modern teaching methods</p>
+        <p class="moveable text30">Fostered a supportive and encouraging classroom environment</p>
+        
+        <h3 class="moveable working__place__title text31">SUBSTITUTE TEACHER</h3>
+        <p class="moveable place__name text32">Gresdale Elementary School, 2011-2012</p>
+        <p class="moveable text33">Substituted for English and Art classes</p>
+        <p class="moveable text34">Provided after-school tutoring for students</p>
+        <p class="moveable text35">Taught English summer school classes for elementary students</p>
+        
+        <h3 class="moveable information__title text36">ACADEMIC BACKGROUND</h3>
+        <h3 class="moveable working__place__title text37">OULCASTER UNIVERSITY</h3>
+        <p class="moveable place__name text38">MA in Education & Development (2010)</p>
+        <p class="moveable text39">Graduated with First Honors</p>
+        <p class="moveable text40">Published a paper entitled Education from a Behavioral Standpoint</p>
+        <p class="moveable text41">Successfully organized a state-funded teaching program</p>
+        
+        <h3 class="moveable working__place__title text42">KIRMARE STATE COLLEGE</h3>
+        <p class="moveable place__name text43">B.A. in English Literature (2007)</p>
+        <p class="moveable text44">Graduated with 1st Honors</p>
+        <p class="moveable text45">Awarded with Academic Excellence in English</p>
+        <p class="moveable text46">Member of the Kirmere Youth Education Charity</p>
+     </aside>
 </div>`;
 
 export default template1;

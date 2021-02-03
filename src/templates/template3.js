7092ddba6dafa3c94005b5784cee9ce8ba700115
Danil Kohan <Sportodavka@gmail.com>
2021-02-03 21:06:53 +0300

    feat: add new class

--- src/templates/template3.js
+++ src/templates/template3.js
@@ -8,8 +8,8 @@ const template3 = `<footer class="container_temp3">
                               </div>
 
                               <div class="moveable profile_person">
-                                   <h3 class="moveable">PERSONAL PROFILE</h3>
-                                   <p class="moveable profile_text">
+                                   <h3 class="text moveable">PERSONAL PROFILE</h3>
+                                   <p class="text moveable profile_text">
                                         A software engineer with hands-on<br>
                                         experience in all levels of testing,<br>
                                         including performance, functional,<br>
@@ -19,27 +19,27 @@ const template3 = `<footer class="container_temp3">
                               </div>
 
                               <div class="moveable know_person">
-                                   <h3 class="moveable know_person_title">AREAS OF EXPERTISE</h3>
+                                   <h3 class="text moveable know_person_title">AREAS OF EXPERTISE</h3>
                               <ul class="know_person_list">
-                                        <li class="moveable">Hands-on SQL experience</li>
-                                        <li class="moveable">Java, Ruby, PHP and Python<br>
+                                        <li class="text moveable">Hands-on SQL experience</li>
+                                        <li class="text moveable">Java, Ruby, PHP and Python<br>
                                             experience</li>
-                                        <li class="moveable">Microsoft ASP.NET MVC, Web<br>
+                                        <li class="text moveable">Microsoft ASP.NET MVC, Web<br>
                                             API experience</li>
-                                        <li class="moveable">Working knowledge of C, C++,<br>
+                                        <li class="text moveable">Working knowledge of C, C++,<br>
                                             and C#</li>
                               </ul>
                               </div>
 
                               <div class="moveable skills_person">
-                                   <h3 class="moveable skills_person_title">OTHER SKILLS</h3>
+                                   <h3 class="text moveable skills_person_title">OTHER SKILLS</h3>
                               <ul class="skills_person_list">
-                                   <li class="moveable">The ability to analyze complex<br>
+                                   <li class="text moveable">The ability to analyze complex<br>
                                        technical information</li>
-                                   <li class="moveable">Can analyze, design and<br>
+                                   <li class="text moveable">Can analyze, design and<br>
                                        implement database structures</li>
-                                   <li class="moveable">Detail oriented</li>
-                                   <li class="moveable">Excellent problem solver</li>
+                                   <li class="text moveable">Detail oriented</li>
+                                   <li class="text moveable">Excellent problem solver</li>
                               </ul>
                               </div>
                          </section>
@@ -47,13 +47,13 @@ const template3 = `<footer class="container_temp3">
 
                           <section class="right_section">
                               <div class="moveable header">
-                                   <h3 class="moveable profession_person">SOFTWARE ENGINEER</h3>
-                                   <h2 class="moveable name_person">CHAD<br>
+                                   <h3 class="text moveable profession_person">SOFTWARE ENGINEER</h3>
+                                   <h2 class="text moveable name_person">CHAD<br>
                                                            GIBBONS</h2>
                               </div>
 
                               <div class="moveable contact_person">
-                              <h3 class="moveable contact_person_title">GET IN CONTACT</h3>
+                              <h3 class="text moveable contact_person_title">GET IN CONTACT</h3>
                             <a class="moveable left__link1" href="tel:+1 234 56 78 90">Number: 123-456-7890</a><br>
                             <a class="moveable left__link2" href="mailto: hello@reallygreatsite.com">Email: hello@reallygreatsite.com</a><br>
                             <a class="moveable left__link3" href="https://www.reallygreatsite.com/" target="_blank">Website: www.reallygreatsite.com</a><br>
@@ -61,41 +61,41 @@ const template3 = `<footer class="container_temp3">
                               </div>
 
                               <div class="moveable work_person">
-                                   <h3 class="moveable work_person_title">WORK EXPERIENCE</h3>
-                                   <p class="moveable profession_name">SOFTWARE ENGINEER</p>
-                                   <p class="moveable work_person_list">Healthmade Solutions | 2015 - Present</p>
+                                   <h3 class="text moveable work_person_title">WORK EXPERIENCE</h3>
+                                   <p class="text moveable profession_name">SOFTWARE ENGINEER</p>
+                                   <p class="text moveable work_person_list">Healthmade Solutions | 2015 - Present</p>
                               <ul>
-                                   <li class="moveable">Directs software design and development</li>
-                                   <li class="moveable">Evaluates interface between hardware and software</li>
-                                   <li class="moveable">Advises customers regarding maintenance of software system</li>
+                                   <li class="text moveable">Directs software design and development</li>
+                                   <li class="text moveable">Evaluates interface between hardware and software</li>
+                                   <li class="text moveable">Advises customers regarding maintenance of software system</li>
                               </ul>  
-                                   <p class="moveable profession_name">JUNIOR ENGINEER</p> 
-                                   <p class="moveable work_person_list">RealPro Software Inc. | 2014 – 2015</p> 
+                                   <p class="text moveable profession_name">JUNIOR ENGINEER</p> 
+                                   <p class="text moveable work_person_list">RealPro Software Inc. | 2014 – 2015</p> 
                               <ul>
-                                   <li class="moveable">Stored, retrieved, and manipulated data</li>
-                                   <li class="moveable">Modified existing software to correct errors and optimize efficiency</li>
-                                   <li class="moveable">Prepared detailed reports</li>
+                                   <li class="text moveable">Stored, retrieved, and manipulated data</li>
+                                   <li class="text moveable">Modified existing software to correct errors and optimize efficiency</li>
+                                   <li class="text moveable">Prepared detailed reports</li>
                               </ul>     
                               </div>
 
                               <div class="moveable history_person">
-                                   <h3 class="moveable history_person_title">EDUCATION HISTORY</h3>
-                                   <p class="moveable place__name">UNIVERSITY OF EL DORADO</p>
-                                   <p class="moveable history_person_list">Bachelor of Software Engineering, 2014</p>
+                                   <h3 class="text moveable history_person_title">EDUCATION HISTORY</h3>
+                                   <p class="text moveable place__name">UNIVERSITY OF EL DORADO</p>
+                                   <p class="text moveable history_person_list">Bachelor of Software Engineering, 2014</p>
                               <ul>
-                                   <li class="moveable">President, Computer and Technology Organization</li>
-                                   <li class="moveable">Vice President, Programming Club</li>
-                                   <li class="moveable">Member, Video Game Society</li>
-                                   <li class="moveable">Member, Student IT Helpdesk</li>
+                                   <li class="text moveable">President, Computer and Technology Organization</li>
+                                   <li class="text moveable">Vice President, Programming Club</li>
+                                   <li class="text moveable">Member, Video Game Society</li>
+                                   <li class="text moveable">Member, Student IT Helpdesk</li>
                               </ul>
-                                   <p class="moveable place__name">BEECHTOWN ACADEMY</p>
-                                   <p class="moveable history_person_list">Graduated Class of 2010. Honor Roll</p>
+                                   <p class="text moveable place__name">BEECHTOWN ACADEMY</p>
+                                   <p class="text moveable history_person_list">Graduated Class of 2010. Honor Roll</p>
                               <ul>
-                                   <li class="moveable">President and Founder, Computer Club</li>
-                                   <li class="moveable">Student Council President</li>
-                                   <li class="moveable">Debate Team Vice President</li>
-                                   <li class="moveable">Member of the Math Team</li>
-                                   <li class="moveable">Member of the Chess Team</li>
+                                   <li class="text moveable">President and Founder, Computer Club</li>
+                                   <li class="text moveable">Student Council President</li>
+                                   <li class="text moveable">Debate Team Vice President</li>
+                                   <li class="text moveable">Member of the Math Team</li>
+                                   <li class="text moveable">Member of the Chess Team</li>
                               </ul>
                               </div>
                          </section>

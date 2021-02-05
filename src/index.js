3d5f7688eaab05a01d05186019305b611d8a1348
wh1teee <hemp0@mail.ru>
2021-02-05 23:08:46 +0300

    refactor: some code refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import DOM from './js/DOMLinks';
 import createMainDOM from './pages/main';
 import createConstructorDOM from './pages/constructor';
 import header from './js/Header';
@@ -6,60 +5,50 @@ import WorkSpace from './js/WorkSpace';
 import workSpaceHeader from './js/WorkSpaceHeader';
 import controls from './js/ControlsIcons';
 import ControlsElements from './js/ControlsElements';
-import editor from './js/Editor';
 import slider from './js/Slider';
 import auth from './js/FirebaseAuth';
-
-
-import {template1, template2} from './templates';
-
 import './styles/main.scss';
-//import './styles/constructor.scss';
-//import './styles/editor.scss';
 import './style.scss';
 import { startMovable } from './js/moveable';
 
-const dom = DOM.getHTMLElements();
 const workSpace = new WorkSpace('resume');
-
 let moveableItems;
-function startMain(ifLoggedIn = false) {
-    M.AutoInit();
-    createMainDOM();
-    header.createMainHeader();
-    auth.createAuthPanelMain(ifLoggedIn);
-    window.removeEventListener('resize', resizeWindowListener);
-    slider.init()
-    document.getElementById('create-design').addEventListener('click', createEditorPage);
+
+function startMain (ifLoggedIn = false) {
+  M.AutoInit();
+  createMainDOM();
+  header.createMainHeader();
+  auth.createAuthPanelMain(ifLoggedIn);
+  window.removeEventListener('resize', resizeWindowListener);
+  slider.init();
+  document.getElementById('create-design').addEventListener('click', createEditorPage);
 }
 
-startMain()
+startMain();
 
-function createEditorPage() {
+function createEditorPage () {
+    const controlsElements = new ControlsElements('Templates');
     createConstructorDOM();
     header.createHeader();
+    workSpaceHeader.createWorkSpaceHeaderRight();
     controls.createControlPanel();
-
-    const controlsElements = new ControlsElements('Templates');
     controlsElements.createControlElementsPanel();
     workSpace.createWorkSpace();
-    workSpaceHeader.createWorkSpaceHeaderRight();
+
+  document.querySelector('.controls__elements-list').addEventListener('click', (event) => {
+
+    workSpace.showTemplateOnScreen(event.target);
+    if (moveableItems.length > 0) {
+      moveableItems[0].updateTarget();
+    }
+  });
 
     window.addEventListener('resize', resizeWindowListener);
-    document.querySelector('.controls__elements-list').addEventListener('click', (event) => {
-
-     workSpace.showTemplateOnScreen(event.target);
-
-        if (moveableItems.length > 0) {
-            moveableItems[0].updateTarget()
-        }
-        // moveableItems[1].findSelectableTargets()
-        // moveableItems = startMovable()  //start moveable
-    });
-    moveableItems = startMovable()  //start moveable
+    moveableItems = startMovable();  //start moveable
 }
 
-function resizeWindowListener(e){
-    workSpace.calculateScale(e)
+function resizeWindowListener (e) {
+  workSpace.calculateScale(e);
 }
-export {moveableItems, createEditorPage, startMain}
+
+export { moveableItems, createEditorPage, startMain };

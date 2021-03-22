afe68dee532aeff33161bb6a8ad47cabc7f4ee2e
wh1teee <hemp0@mail.ru>
2021-03-22 17:18:33 +0300

    refactor: rewrite header panel for bootstrap

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,12 +1,10 @@
-import createMainDOM from '../pages/main';
 import html2canvas from 'html2canvas';
 import Canvas2Image from 'wd-canvas2image';
 import html2PDF from 'jspdf-html2canvas';
 import auth from './FirebaseAuth';
-import slider from './Slider';
 import '../styles/constructor.scss';
 import DOM from './DOMLinks';
-import { startMain } from '../index';
+import { createMain } from '../index';
 
 const menuElements = ['< Main', 'File'];
 const mainMenuElements = ['Home', 'Features', 'Learning'];
@@ -16,12 +14,19 @@ class Header {
   createMainHeader () {
     const dom = DOM.getHTMLElements();
 
-    mainMenuElements.map(item => {
+    mainMenuElements.map((item, i) => {
+      console.log(11);
       const listItem = document.createElement('li');
+      listItem.classList.add('nav-item');
       listItem.innerHTML = `
-            <a class="waves-effect waves-light btn modal-trigger" href="#modal2">${item}
+            <a 
+            class="nav-link btn btn-lg ${i === 0 ? 'active' : ''}" 
+            type="button" 
+            data-bs-toggle="modal" 
+            data-bs-target="#developingModal">${item}
             <span class="material-icons">keyboard_arrow_down</span></a>
             `;
+
       dom.menuControlsList.append(listItem);
     });
   }
@@ -32,33 +37,38 @@ class Header {
     menuElements.map(item => {
       const listItem = document.createElement('li');
       listItem.innerHTML = `
-            <a class="waves-effect waves-light btn transparent no-shadow">${item}</a>
+            <a class="btn btn-lg no-shadow">${item}</a>
             `;
       dom.menuControlsList.append(listItem);
     });
 
     dom.headerControls.innerHTML = `
-            <li><a href="#modal-saveToCloud" class='waves-effect waves-light btn modal-trigger transparent no-shadow' id='download-button0'>Save to cloud</a></li>
-            <li><a class='waves-effect waves-light btn transparent no-shadow' id='download-button1'>Save as img</a></li>
-            <li><a class='waves-effect waves-light btn transparent no-shadow' id='download-button2'>Save as pdf</a></li>
-            
-            <!-- SIGN UP MODAL -->
-             <div id="modal-saveToCloud" class="modal">
+            <li><a class='btn no-shadow' id='download-button0' type="button" data-bs-toggle="modal" data-bs-target="#saveToCloudModal">Save to cloud</a></li>
+            <li><a class='btn no-shadow' id='download-button1'>Save as img</a></li>
+            <li><a class='btn no-shadow' id='download-button2'>Save as pdf</a></li>            
+            <div class="modal fade" id="saveToCloudModal" tabindex="-1" aria-labelledby="saveToCloudModal" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                <h4>Input image name</h4><br />
-                <form id="save-form">
-                    <div class="input-field">
-                        <input type="text" id="save-name" required />
-                        <label for="save-name">Image name</label>
-                    </div>
-                    <button class="white-text flow-text waves-effect waves-light btn orange lighten-2 z-depth-0">Submit</button>
-                </form>
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Save image to cloud</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <form id="save-form">
+                        <label for="save-name"></label>
+                        <input type="text" class="form-control" id="save-name" placeholder="Image name" required>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-lg btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-lg btn-primary" form="save-form">Save</button>
+                  </div>
                 </div>
+              </div>
             </div>
         `;
 
     document.getElementById('download-button0').addEventListener('click', function () {
-      const saveModal = document.querySelector('#modal-saveToCloud');
 
       const saveForm = document.querySelector('#save-form');
       saveForm.addEventListener('submit', (e) => {
@@ -66,7 +76,8 @@ class Header {
         const imgName = saveForm['save-name'].value;
 
         html2canvas(document.querySelector('.sheet__container')).then(function (canvas) {
-          M.Modal.getInstance(saveModal).close();
+          const myModalEl = document.getElementById('saveToCloudModal');
+          bootstrap.Modal.getInstance(myModalEl).hide();
           saveForm.reset();
           auth.saveImgToCloud(canvas, imgName);
         });

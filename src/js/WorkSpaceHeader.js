7a19c6e873bb8bc6e1aee99494f4b17c3f658202
Konstantsin Petrovskiy <wh1teee1337lol@gmail.com>
2021-04-24 17:40:13 +0300

    refactor: redesigned for event delegation

--- src/js/WorkSpaceHeader.js
+++ src/js/WorkSpaceHeader.js
@@ -1,62 +1,80 @@
 import DOM from './DOMLinks';
-import {template1, template2} from '../templates';
-import {moveableItems} from '../index';
+import { moveableItems } from '../index';
 
 let canvas = document.createElement('canvas');
 canvas.setAttribute('class', 'canvas');
 
 class WorkSpaceHeader {
-    createWorkSpaceHeaderRight(){
-        const dom = DOM.getHTMLElements();
-        dom.workSpaceHeaderRight.innerHTML = `
+  createWorkSpaceHeaderRight () {
+    const dom = DOM.getHTMLElements();
+    dom.workSpaceHeaderRight.innerHTML = `
         <span class='material-icons' id='delete__element'>delete_outline</span>
         <span class='material-icons' id='delete__all'>delete</span>
         <span class="material-icons" id="volume">volume_off</span>
         `;
-        document.getElementById('delete__all').addEventListener('click', function() {
-            const dom = DOM.getHTMLElements();
-            dom.sheetContainer.innerHTML = '';
-            dom.sheetContainer.style.backgroundColor = 'white';
-            dom.sheetContainer.style.backgroundImage = 'none';
-            moveableItems[0].updateRect()
+    dom.workSpaceHeaderRight.addEventListener('click', workSpaceHeaderRightListeners);
+
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Delete') {
+        if (!moveableItems[0].target) return; // if there are no selected objects
+        moveableItems[0].target.forEach(el => {
+          el.remove();
         });
-        document.getElementById('delete__element').addEventListener('click', () => {
-            if (!moveableItems[0].target) return; // if there are no selected objects
-            moveableItems[0].target.forEach( el => {
-                el.remove()
-            })
-            moveableItems[0].updateRect()
-            dom.workSpaceHeaderLeft.innerHTML = ''
-        })
-        document.addEventListener('keydown', (e) => {
-            if (e.code === "Delete") {
-                if (!moveableItems[0].target) return; // if there are no selected objects
-                moveableItems[0].target.forEach( el => {
-                    el.remove()
-                })
-                moveableItems[0].updateRect()
-                dom.workSpaceHeaderLeft.innerHTML = ''
-            }
-        })
-        document.getElementById('volume').addEventListener('click', (e) => {
-            if (e.target.textContent === 'volume_off') {
-                e.target.textContent = 'volume_up'
-                document.addEventListener('click', clickSound)
-                document.addEventListener('keydown', clickSound)
-            } else {
-                e.target.textContent = 'volume_off'
-                document.removeEventListener('click', clickSound)
-                document.removeEventListener('keydown', clickSound)
-            }
-        })
+        moveableItems[0].updateRect();
+        dom.workSpaceHeaderLeft.innerHTML = '';
+      }
+    });
 
-        function clickSound() {
-            document.querySelector('audio').play()
-        }
+    function workSpaceHeaderRightListeners (e) {
+      const idOfElement = e.target.getAttribute('id') || null;
+
+      if (idOfElement === 'delete__all') {
+        deleteAll();
+      }
+      if (idOfElement === 'delete__element') {
+        deleteElement();
+      }
+      if (idOfElement === 'volume') {
+        changeSoundOfClicks(e);
+      }
     }
 
-    createWorkSpaceHeaderLeft(){
+    function deleteAll () {
+      const dom = DOM.getHTMLElements();
+      dom.sheetContainer.innerHTML = '';
+      dom.sheetContainer.style.backgroundColor = 'white';
+      dom.sheetContainer.style.backgroundImage = 'none';
+      moveableItems[0].updateRect();
     }
+
+   function deleteElement () {
+      if (!moveableItems[0].target) return; // if there are no selected objects
+      moveableItems[0].target.forEach(el => {
+        el.remove();
+      });
+      moveableItems[0].updateRect();
+      dom.workSpaceHeaderLeft.innerHTML = '';
+    }
+
+   function changeSoundOfClicks (e){
+      if (e.target.textContent === 'volume_off') {
+        e.target.textContent = 'volume_up';
+        document.addEventListener('click', clickSound);
+        document.addEventListener('keydown', clickSound);
+      } else {
+        e.target.textContent = 'volume_off';
+        document.removeEventListener('click', clickSound);
+        document.removeEventListener('keydown', clickSound);
+      }
+    }
+
+    function clickSound () {
+      document.querySelector('audio').play();
+    }
+  }
+
+  createWorkSpaceHeaderLeft () {
+  }
 }
 
 const workSpaceHeader = new WorkSpaceHeader;
